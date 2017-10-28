89f4ce432cfe2d9efbbbde6a13338f67e63fd09f
Logan <logansrooks@gmail.com>
2017-10-28 14:08:10 -0400

    added optional arguments

--- pursuit.m
+++ pursuit.m
@@ -1,7 +1,16 @@
-function [x, y, theta, t] = pursuit(startPose, path, Kh, Kv, Ki)
-    length = 0.1;
-    distance = 0.01;
-    h = 0.01;
+function [x, y, theta, t] = pursuit(startPose, path, optionalArgs)
+    % startPose is of form [x, y, theta] and path is an anonymous function
+    % Optional arguments have form {Kh, Kv, Ki, h, simTime, length}
+    
+    numOptionalArgs = length(optionalArgs);
+    if numOptionalArgs > 6
+        error('pursuit:TooManyInputs', ...
+        'requires at most 6 optional inputs');
+    end
+    
+    defaultOptionalArgs = {10, 2.5, 0.01, 0.01, 10, 0.1};
+    defaultOptionalArgs(1:numOptionalArgs) = optionalArgs;
+    [Kh, Kv, Ki, h, simTime, length] = defaultOptionalArgs{:};
     
     x = [startPose(1)];
     y = [startPose(2)];
@@ -15,14 +24,14 @@ function [x, y, theta, t] = pursuit(startPose, path, Kh, Kv, Ki)
     yPath = path(xPath);
     relativePose = [xPath - x(k), yPath - y(k), atan((yPath - y(k))/(xPath - x(k)))];
     
-    for k = 2:2*pi/h
+    for k = 2:simTime/h
         xPath = k*h;
         yPath = path(xPath);
         
-        error = sqrt(relativePose(1)^2 + relativePose(2)^2) - distance;
-        integral = integral + error;
+        positionError = sqrt(relativePose(1)^2 + relativePose(2)^2) - distance;
+        integral = integral + positionError;
         
-        v = Kv*error + Ki*integral;
+        v = Kv*positionError + Ki*integral;
         v = min(max(v,0), 2);
         gamma = Kh*wrapToPi(relativePose(3) - theta(k-1));
         gamma = min(max(gamma,-pi/2), pi/2);
